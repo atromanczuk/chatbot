458bcbafb7eec64fe349dd7e80f1ef22bd6ee308
Tamara Romanczuk <tromanczuk@intecsoft.com.ar>
2022-05-24 18:48:53 -0300

    init: plantilla html + chatbot a mejorar

---
+++ WebChat/WebChat/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    $(".chat_on").click(function () {
+        $(".Layout").toggle();
+        $(".chat_on").hide(300);
+    });
+
+    $(".chat_close_icon").click(function () {
+        $(".Layout").hide();
+        $(".chat_on").show(300);
+    });
+
+});
